95548fb541e063855839ced59ef57820957845c5
DevSouza <devsouza01@gmail.com>
2020-08-16 14:28:54 -0300

    Enviando uma falha no tdd

--- src/test/java/br/com/convexus/ConvexusApplicationTests.java
+++ src/test/java/br/com/convexus/ConvexusApplicationTests.java
@@ -1,5 +1,7 @@
 package br.com.convexus;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class ConvexusApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertTrue(false);
 	}
 
 }
