b6c845f5bf04f0f94b8915f7f8b7a8db77d3a014
DevSouza <devsouza01@gmail.com>
2020-08-16 00:11:00 -0300

    teste

---
+++ src/main/java/br/com/convexus/ConvexusApplication.java
@@ -0,0 +1,13 @@
+package br.com.convexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/convexus/ConvexusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.convexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
